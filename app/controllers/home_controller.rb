473544ec0bbb0a6a4b418fcf7df60ffa26a190eb
Charlyn G <chardane@gmail.com>
2016-05-07 01:26:57 -0700

    edit the view, controller and css to show tags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+    get_tags if params[:image_url].present?
+  end
+
+  private
+
+  def get_tags
+    # Get tags for the image given from Clarifai
+    @tag_response = ClarifaiRuby::TagRequest.new.get(params[:image_url])
+
+    # Extract out just the words from the tags
+    @tags = @tag_response.tag_images.first.tags_by_words
+
+    # Save the image url so we can access it later
+    @image_url = params[:image_url]
   end
 end
